4011560c374236901b9f7e0239a940b80e08c456
o <laiquanghuy24122001@gmail.com>
2026-02-27 09:31:42 +0700

    feat: add ConvexErrorBoundary component with structured error display

--- packages/lazyconvex/src/components/index.ts
+++ packages/lazyconvex/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as EditorsSection } from './editors-section'
+export { default as ConvexErrorBoundary } from './error-boundary'
 export { fields, FormContext } from './fields'
 export type { Api } from './fields'
 export { FileApiContext, FileApiProvider } from './file-field'
